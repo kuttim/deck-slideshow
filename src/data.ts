08db7d43e1d38e063c983cd546f178b6e93c3108
kuttim <kuttim.mikk@gmail.com>
2022-05-28 15:46:04 +0300

    add more slide data

--- src/data.ts
+++ src/data.ts
@@ -10,13 +10,43 @@ const cover = {
 const slides = [
   {
     title: 'Blue art',
-    content:
-      'If youre looking for the shade of-the-moment, many artists are gravitating towards art with tones of blue. says nail artist Queenie Nguyen, who recently constructed this negative-space design with a cobalt blue OPI polish. Its both calming and refreshing at the same time.',
+    content: `If you're looking for the shade of-the-moment, many artists are gravitating towards art with tones of blue. "2022 is all about 50 shades of blues," says nail artist Queenie Nguyen, who recently constructed this negative-space design with a cobalt blue OPI polish. It's both calming and refreshing at the same time.`
   },
   {
-    title: 'Green art',
-    content: 'If youre looking for the shade of-the-moment, many artists are gravitating towards art with tones of blue. says nail artist Queenie Nguyen, who recently constructed this negative-space design with a cobalt blue OPI polish. Its both calming and refreshing at the same time.',
-
+    title: 'Embellished French',
+    content: `"Embellished French manicures are everywhere right now. Think of it as a typical French manicure, perhaps in a fun color, with some other design element accompanying it. This trend is a fun way to spice up an otherwise-ordinary nail look." - Emily H. Rudman, CEO and Founder of Emilie Heathe`
+  },
+  {
+    title: 'Hoops & Charms',
+    content:  `"Crazy add-ons, such as hoops and nail charms, will make a strong appearance across manicures in 2022. When it comes to our nails, we don’t necessarily prioritize practicality over making a bold statement. Choosing a nail design that you want to show off to everyone you come in contact with will always spark joy — or, at the very minimum, a conversation." - Chaun Legend, Celebrity Nail Artist`
+  },
+  {
+    title: 'Bright Swirl',
+    content: `"I believe the swirl nail art trend will continue to be popular throughout 2022. It's fun and versatile: it can either be muted, with earth-tone colors, or really fun, if done with bright, vibrant colors. Depending on the colors you use, this trend can be worn in any  season." - Alexa Liberatore, Nail Artist and Founder of Facile Nails`
+  },
+  {
+    title: 'Combo Art',
+    content: `"The combo of different art on every nail is big! We will see a lot of  fun colors and pattern combos, whether it be with nail decals, press-ons, or hand painted art, all in the same manicure. The options are endless, which allows people to let loose with their creativity." - Liberatore`
+  },
+  {
+    title: 'Hot Red',
+    content: `"A clean, bold-colored manicure — like hot pink or red — brings a sense of fun and vibrancy back to people's lives. We've been living through some tough times, and nail art has been a highlight for many of my clients. Their love and appreciation for nail art has grown, I believe, after the salon shutdowns of 2020. Some of my customers have told me that 'nail day' is the highlight of their month.`
+  },
+  {
+    title: 'Confetti Glitter',
+    content: `"I hope to see confetti-style glitter looks start trending in 2022. They are just so festive, fun, and go with any ‘theme’ or season. Last year was all about neutral/muted tones, so this year I'm hoping for something a bit more bold and bright to bring happiness to everyone!" - Rebecca Ludwig, Nail Artist and Educator`
+  },
+  {
+    title: 'Two-Toned French',
+    content: `"A two-toned French manicure is an easy way to step up a nail your nail game and take it to the next level! It's so versatile, you can pair it with almost any color." - Brittney Boyce, Celebrity Nail Artist and Founder of Nails of L.A.`
+  },
+  {
+    title: 'Pink & Black',
+    content: `"I love it when boys defy the norm and wear pink! It's a really excellent contrast color that looks great as a base with simple black nail art to create that extra pop. I created this look using UN/DN LAQR polish in Party Favor and Depressionist." - Boyce`
+  },
+  {
+    title: 'Earthy Greens',
+    content: `"Right now, I've been drawn to gradients of green — nods to new starts, growth in winter, and the beautiful range that exists in a single color. [It's] a theme we explored earlier this season with a JH polish collaboration with the talented @sojinails, who designed an otherworldly interpretation of JH greens with a swirled, three-dimensional layering with our colors Eames, Patina, Artichoke, and Compost.”`
   },
 ];
 
